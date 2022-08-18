a36055c613946ac26df5696a6a8709c4a49eb129
bahadur1996 <bahadur.arshad@gmail.com>
2022-08-18 21:06:38 +0600

    add project to git

---
+++ src/main/java/com/messaging/server/ChatServer.java
@@ -0,0 +1,49 @@
+package com.messaging.server;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public final class ChatServer {
+    static final int port = 8090;
+
+    public static void main(String[] args) {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline channelPipeline = socketChannel.pipeline();
+                        channelPipeline.addLast(new StringDecoder());
+                        channelPipeline.addLast(new StringEncoder());
+
+                        channelPipeline.addLast(new ChatServerHandler());
+                    }
+                });
+
+        try {
+            ChannelFuture channelFuture = bootstrap.bind(port).sync();
+
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
